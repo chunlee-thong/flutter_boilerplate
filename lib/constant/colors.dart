80b1966c61e40168bd4b364004eed323bedce689
chunleethong <thong.chunlee@optistech.com>
2020-04-24 13:05:29 +0700

    update

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../utils/theme_helper.dart';
+import 'package:jin_widget_helper/jin_widget_helper.dart';
 
 const Color white = Color.fromRGBO(0, 0, 0, 1);
 
-MaterialColor primaryColor = hexColorToMaterialColor(0xFF9efcff);
-MaterialColor secondaryColor = hexColorToMaterialColor(0xFFfaaa81);
+MaterialColor primaryColor = ColorUtils.hexColorToMaterialColor(0xFF9efcff);
+MaterialColor secondaryColor = ColorUtils.hexColorToMaterialColor(0xFFfaaa81);
--- lib/widgets/UI_helper.dart
+++ lib/widgets/UI_helper.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:jin_widget_helper/jin_widget_helper.dart';
 
 class UIHelper {
-  static Future<T> showGeneralMessageDialog<T>(BuildContext context, String message) {
+  static Future<T> showGeneralMessageDialog<T>(
+      BuildContext context, String message) {
     return showDialog(
       context: context,
       builder: (context) => GeneralMessageDialog(
