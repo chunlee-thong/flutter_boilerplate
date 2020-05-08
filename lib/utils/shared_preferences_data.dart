f743045bc49e06e9aaf43daea618208c8ec376e2
chunleethong <thong.chunlee@optistech.com>
2020-05-08 14:53:03 +0700

    Update shared_preferences_data.dart

--- lib/utils/shared_preferences_data.dart
+++ lib/utils/shared_preferences_data.dart
@@ -1,11 +1,13 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+const LOGIN_KEY = "isLoggedIn";
+
 Future<bool> saveLoggedInStatus(bool status) async {
   final pref = await SharedPreferences.getInstance();
-  return pref.setBool("isLoggedIn", status);
+  return pref.setBool(LOGIN_KEY, status);
 }
 
 Future<bool> getLoggedInStatus() async {
   final pref = await SharedPreferences.getInstance();
-  return pref.getBool("isLoggedIn") ?? false;
+  return pref.getBool(LOGIN_KEY) ?? false;
 }
