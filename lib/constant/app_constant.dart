0b516f099426754cdeb49e40254594f0133486d1
chunleethong <thong.chunlee@optistech.com>
2020-06-23 14:06:51 +0700

    Update app_constant.dart

--- lib/constant/app_constant.dart
+++ lib/constant/app_constant.dart
@@ -1,4 +1,5 @@
 class AppConstant {
-  static const String BASE_URL = "https://stripe-panda-api.herokuapp.com";
+  static const String BASE_URL =
+      "https://chunlee-node-api-boilerplate.herokuapp.com";
   static const String APP_VERSION = "1.0.0";
 }
