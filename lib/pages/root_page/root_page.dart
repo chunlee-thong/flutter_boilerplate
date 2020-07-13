5b5eb75d82105bccf51770df192de75fb203d5c9
Chunlee Thong <44610456+Chunlee17@users.noreply.github.com>
2020-07-13 21:47:30 +0700

    Update root_page.dart

--- lib/pages/root_page/root_page.dart
+++ lib/pages/root_page/root_page.dart
@@ -36,39 +36,48 @@ class _RootPageState extends State<RootPage> {
     return ChangeNotifierProvider(
       create: (_) => BottomNavigationProvider(widget.startPageIndex),
       child: Consumer<BottomNavigationProvider>(
-        builder: (context, state, child) => Scaffold(
-          body: PageView.builder(
-            itemCount: pages.length,
-            controller: state.pageController,
-            physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return pages[index];
-            },
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: state.currentIndex,
-            type: BottomNavigationBarType.fixed,
-            onTap: (index) {
-              state.changeIndex(index);
-            },
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text("Home"),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.assessment),
-                title: Text("Report"),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.map),
-                title: Text("Map"),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                title: Text("Setting"),
-              )
-            ],
+        builder: (context, state, child) => WillPopScope(
+          onWillPop: () async {
+            if (state.currentIndex != 0) {
+              state.changeIndex(0);
+              return false;
+            }
+            return true;
+          },
+          child: Scaffold(
+            body: PageView.builder(
+              itemCount: pages.length,
+              controller: state.pageController,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return pages[index];
+              },
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: state.currentIndex,
+              type: BottomNavigationBarType.fixed,
+              onTap: (index) {
+                state.changeIndex(index);
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  title: Text("Home"),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.assessment),
+                  title: Text("Report"),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.map),
+                  title: Text("Map"),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  title: Text("Setting"),
+                )
+              ],
+            ),
           ),
         ),
       ),
