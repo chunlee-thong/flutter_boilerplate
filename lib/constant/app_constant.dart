d02fc2c599f9550855a8f19ea6b687cadadbc025
chunleethong <thong.chunlee@optistech.com>
2020-07-24 10:25:49 +0700

    update locale path

--- lib/constant/app_constant.dart
+++ lib/constant/app_constant.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
+
 class AppConstant {
   static const String BASE_URL = "https://jsonplaceholder.typicode.com";
   static const String APP_VERSION = "1.0.0";
 }
+
+final EN_LOCALE = Locale('en', 'US');
+final KH_LOCALE = Locale('km', 'KH');
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import './constant/app_constant.dart';
 import './api_service/mock_api_provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
@@ -18,7 +19,7 @@ void main() {
   return runApp(
     EasyLocalization(
       child: MyApp(),
-      supportedLocales: [Locale('en', 'US'), Locale('km', 'KH')],
+      supportedLocales: [EN_LOCALE, KH_LOCALE],
       path: 'resources/language',
     ),
   );
