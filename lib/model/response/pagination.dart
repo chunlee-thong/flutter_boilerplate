2d28ae804fad89d9665851f9ad294a518b56e56f
chunleethong <thong.chunlee@optistech.com>
2020-10-14 16:31:38 +0700

    cleaning structure for pagination

---
+++ lib/model/response/pagination.dart
@@ -0,0 +1,17 @@
+class Pagination {
+  Pagination({
+    this.page,
+    this.totalItems,
+    this.totalPage,
+  });
+
+  int page;
+  int totalItems;
+  int totalPage;
+
+  factory Pagination.fromJson(Map<String, dynamic> json) => Pagination(
+        page: json["page"] == null ? null : json["page"],
+        totalItems: json["total_items"] == null ? null : json["total_items"],
+        totalPage: json["total_page"] == null ? null : json["total_page"],
+      );
+}
