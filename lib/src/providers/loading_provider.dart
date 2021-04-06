7d14fa7b5d7103fc6481bf1950f2287d6a928f95
Chunlee Thong <chunlee.thong@gmail.com>
2021-04-06 11:42:52 +0700

    Update loading_provider.dart

--- lib/src/providers/loading_provider.dart
+++ lib/src/providers/loading_provider.dart
@@ -4,12 +4,12 @@ import 'package:provider/provider.dart';
 class LoadingProvider extends ChangeNotifier {
   bool isLoading = false;
 
-  static void toggleLoading(BuildContext context) {
-    Provider.of<LoadingProvider>(context, listen: false)._toggleLoading();
+  static void toggleLoading(BuildContext context,[bool value]) {
+    Provider.of<LoadingProvider>(context, listen: false)._toggleLoading(value);
   }
 
-  void _toggleLoading() {
-    isLoading = !isLoading;
+  void _toggleLoading([bool value]) {
+    isLoading = value??!isLoading;
     notifyListeners();
   }
 }
