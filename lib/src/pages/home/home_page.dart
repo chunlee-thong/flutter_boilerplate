0e16b2d14f6e57755849519012d17cbb04439f23
Chunlee Thong <chunlee.thong@gmail.com>
2021-08-21 16:57:31 +0700

    update toast package

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -2,11 +2,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 import '../../constant/app_config.dart';
+import '../../constant/locale_keys.dart';
+import '../../providers/theme_provider.dart';
 import '../../widgets/buttons/primary_button.dart';
 import '../../widgets/dialog/language_picker_dialog.dart';
 import '../../widgets/ui_helper.dart';
-import '../../providers/theme_provider.dart';
-import '../../constant/locale_keys.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,19 +19,40 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: UIHelper.CustomAppBar(title: AppConfig.APP_NAME),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             PrimaryButton(
-              fullWidth: false,
-              margin: const EdgeInsets.all(16),
               onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (context) => const LanguagePickerDialog());
+                showDialog(context: context, builder: (context) => const LanguagePickerDialog());
               },
               child: Text(tr(LocaleKeys.change_language)),
+            ),
+            PrimaryButton(
+              onPressed: () {
+                UIHelper.showErrorDialog(context, "This is an error dialog");
+              },
+              child: const Text("Show error dialog"),
+            ),
+            PrimaryButton(
+              onPressed: () {
+                UIHelper.showMessageDialog(context, "This is a simple dialog");
+              },
+              child: const Text("Show Simple dialog"),
+            ),
+            PrimaryButton(
+              onPressed: () {
+                UIHelper.showSnackBar(context, "This is a SnackBar");
+              },
+              child: const Text("Show SnackBar"),
+            ),
+            PrimaryButton(
+              onPressed: () {
+                UIHelper.showToast(context, "This is a Toast");
+              },
+              child: const Text("Show Toast"),
             ),
           ],
         ),
--- lib/src/widgets/ui_helper.dart
+++ lib/src/widgets/ui_helper.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart' as StyledToast;
 
 import 'dialog/message_dialog.dart';
 
 class UIHelper {
-  static Future<T?> showMessageDialog<T>(
-      BuildContext context, dynamic message) async {
+  static Future<T?> showMessageDialog<T>(BuildContext context, dynamic message) async {
     return await showDialog(
       context: context,
       builder: (context) => CustomMessageDialog(
@@ -15,8 +14,7 @@ class UIHelper {
     );
   }
 
-  static Future<T?> showErrorDialog<T>(
-      BuildContext context, dynamic message) async {
+  static Future<T?> showErrorDialog<T>(BuildContext context, dynamic message) async {
     return await showDialog(
       context: context,
       builder: (context) => CustomMessageDialog.error(
@@ -26,24 +24,53 @@ class UIHelper {
     );
   }
 
-  static Future<T?> showCustomDialog<T>(
-      BuildContext context, Widget dialog) async {
+  static Future<T?> showCustomDialog<T>(BuildContext context, Widget dialog) async {
     return await showDialog(
       context: context,
       builder: (context) => dialog,
     );
   }
 
-  ///BuildContext isn't use, but required just in case something change
   static Future showToast(BuildContext context, dynamic message) async {
-    await Fluttertoast.showToast(
-      msg: message.toString(),
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
-      timeInSecForIosWeb: 1,
-      backgroundColor: Colors.black,
-      textColor: Colors.white,
-      fontSize: 16.0,
+    StyledToast.showToastWidget(
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.black54,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        margin: const EdgeInsets.only(bottom: 64),
+        child: Text(message.toString(), style: const TextStyle(color: Colors.white)),
+      ),
+      context: context,
+      animDuration: Duration.zero,
+      dismissOtherToast: true,
+    );
+  }
+
+  static Future showTopSnackBar(BuildContext context, dynamic message) async {
+    StyledToast.showToastWidget(
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        margin: const EdgeInsets.only(top: 64, left: 8, right: 8),
+        child: Text(
+          message.toString(),
+          style: const TextStyle(color: Colors.white, fontSize: 14),
+        ),
+        width: double.infinity,
+      ),
+      context: context,
+      animDuration: const Duration(milliseconds: 200),
+      duration: const Duration(seconds: 3),
+      position: StyledToast.StyledToastPosition.top,
+      animationBuilder: (context, controller, duration, child) {
+        return FadeTransition(opacity: controller, child: child);
+      },
+      dismissOtherToast: true,
     );
   }
 
@@ -54,6 +81,7 @@ class UIHelper {
         content: Text(message.toString()),
         margin: const EdgeInsets.all(8),
         behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.red,
       ),
     );
   }
