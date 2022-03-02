8e58977ab7b1b3edf4c593e01628db0bbf1c7add
Chunlee Thong <44610456+chunlee-thong@users.noreply.github.com>
2022-03-02 16:45:51 +0700

    add pagination handler

--- lib/src/models/response/pagination.dart
+++ lib/src/models/response/pagination.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/rendering.dart';
+import 'package:sura_manager/sura_manager.dart';
+
 class Pagination {
   Pagination({
     required this.page,
@@ -15,3 +18,33 @@ class Pagination {
         totalPage: json["total_page"] ?? 0,
       );
 }
+
+class PaginationResponse<T> {
+  final int totalRecord;
+  List<T> data;
+  PaginationResponse(this.totalRecord, this.data);
+}
+
+class PaginationHandler<T extends PaginationResponse<M>, M extends Object> {
+  bool hasMoreData = true;
+  int page = 1;
+
+  final FutureManager<T> manager;
+  PaginationHandler(this.manager);
+
+  void reset() {
+    hasMoreData = true;
+    page = 1;
+  }
+
+  T handle(T response) {
+    if (manager.hasData) {
+      response.data = [...manager.data!.data, ...response.data];
+    }
+    hasMoreData = response.data.length < response.totalRecord && response.data.isNotEmpty;
+    page += 1;
+    debugPrint("${manager.data.runtimeType} total length: ${response.data.length}");
+    return response;
+  }
+}
+
